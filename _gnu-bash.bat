1b6826afc8c13d1d9c029e84667190f1dad4a034
Carlos Pardo <carlos@picuino.com>
2021-05-09 21:28:28 +0200

    Añadido comando latex

--- _gnu-bash.bat
+++ _gnu-bash.bat
@@ -12,5 +12,6 @@ set PATH=\Bin\7-Zip;%PATH%
 set PATH=\Bin\imagetools;%PATH%
 set PATH=\Bin\xpdf;%PATH%
 set PATH=\Bin\Python39;%PATH%
+set PATH=\Bin\Miktex\miktex\bin;%PATH%
 
 bash.exe %1 %2 %3 %4 %5
