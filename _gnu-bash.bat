f25c1fa4498d01ed1aa2aa97d780302702a5e084
Carlos Pardo <carlos@picuino.com>
2024-07-05 19:44:30 +0200

    05/07/2024 Update

--- _gnu-bash.bat
+++ _gnu-bash.bat
@@ -1,10 +1,8 @@
 @echo off
-set PATH=
 set PATH=\Bin\cygwin64\bin;%PATH%
 set PATH=%~dp0\venv\Scripts;%PATH%
 call activate.bat
 
-set PATH=\Bin\Python311;%PATH%
 set PATH=\Bin\phantomjs\bin;%PATH%
 set PATH=\Bin\ghostscript\bin;%PATH%
 set PATH=\Bin\Java\bin;%PATH%
@@ -13,6 +11,6 @@ set PATH=\Bin\ImageMagick;%PATH%
 set PATH=\Bin\7-Zip;%PATH%
 set PATH=\Bin\imagetools;%PATH%
 set PATH=\Bin\xpdf;%PATH%
-set PATH=\Bin\Miktex\miktex\bin;%PATH%
+set PATH=\Bin\miktex\texmfs\install\miktex\bin\x64;%PATH%
 
 bash.exe %1 %2 %3 %4 %5
