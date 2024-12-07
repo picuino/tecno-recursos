eb92898f64e5d85a353e8f5be2e5c06a7c6b57b4
Carlos Pardo <carlos@picuino.com>
2024-12-07 11:42:28 +0100

    07/12/2024 Update

--- _make-gettext.bat
+++ _make-gettext.bat
@@ -7,8 +7,6 @@ set PATH=\Bin\cygwin64\bin;%PATH%
 set PATH=%~dp0\venv\Scripts;%PATH%
 call activate.bat
 
-cp -f source/_custom/sphinx_rtd_theme/locale/en/LC_MESSAGES/sphinx.po source/_custom/sphinx_rtd_theme/locale/en/sphinx.po 
-
 sphinx-build -b gettext source _build/gettext
 
 cd source
