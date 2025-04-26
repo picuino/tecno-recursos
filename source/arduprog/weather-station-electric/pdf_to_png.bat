bb15f71a54943832498b6a124d2157746e8b62d6
Carlos Pardo <carlos@picuino.com>
2025-04-26 20:06:16 +0200

    26/04/2025 Update

--- source/arduprog/weather-station-electric/pdf_to_png.bat
+++ source/arduprog/weather-station-electric/pdf_to_png.bat
@@ -1,4 +1,5 @@
 @set PATH=\Bin\xpdf;%PATH%
 
 pdftopng -cfg /Bin/xpdf/.xpdfrc -gray -r 200 -aa yes -aaVector yes -freetype yes weather-station-electric.pdf out
+del weather-station-electric.png
 rename out-000001.png weather-station-electric.png
